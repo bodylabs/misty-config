355556004fa6c1b0f6ebff953fd1b8fa5f257ed4
Guillaume Marceau <guillaume.marceau@bodylabs.com>
2016-05-18 14:49:01 -0400

    Find the config dir the same way as node-config

--- webpack-loader.js
+++ webpack-loader.js
@@ -4,18 +4,32 @@ const util = require('util'),
       recursive = require('recursive-readdir-sync'),
       config = require('./index');
 
+//
+// At build time, the configs are parsed from hjson to json,
+// cascaded, and the environment variables are resolved.
+//
+// The resulting POJO is inserted into the build.
+//
 module.exports = function () {
 
-    //
-    // At build time, the configs are parsed from hjson to json,
-    // cascaded, and the environment variables are resolved.
-    //
-    // The resulting POJO is inserted into the build.
-    //
-    var configFiles = recursive(path.join(__dirname, '/../config'));
-    configFiles.push(path.resolve('../config/local.hjson'));
+    // Find the config directory the same way as `node-config` does it:
+    CONFIG_DIR = config.util.initParam('NODE_CONFIG_DIR', path.join( process.cwd(), 'config') );
+    if (CONFIG_DIR.indexOf('.') === 0) {
+        CONFIG_DIR = path.join(process.cwd(), CONFIG_DIR);
+    }
 
-    configFiles.forEach(this.addDependency);
+    // Declare all config file present as dependencies...
+    recursive(CONFIG_DIR).forEach(this.addDependency);
+
+    // ...as well as all possible local file, since they could be
+    // added any moment
+    var extNames = ['js', 'json', 'json5', 'hjson', 'toml',
+                    'coffee', 'iced', 'yaml', 'yml', 'cson', 'properties'];
+    for(ext of extNames) {
+        var filename = path.resolve(CONFIG_DIR, 'local.' + ext);
+        this.addDependency(filename);
+    }
+
     this.cacheable();
 
     return `module.exports = ${JSON.stringify(config)}`;
